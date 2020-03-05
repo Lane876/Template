cede085425683c927e992378983705b71953028a
Lane876 <milenkovicmilan10@gmail.com>
2020-03-05 17:59:51 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route } from "react-router-dom";
+import { gsap } from "gsap";
+import "./styles/App.scss";
+import Header from "./components/header";
+import Navigation from "./components/navigation";
+
+import CaseStudies from "./pages/CaseStudies";
+import Approach from "./pages/Approach";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/case-studies", name: "caseStudies", Component: CaseStudies },
+  { path: "/approach", name: "approach", Component: Approach },
+  { path: "/services", name: "services", Component: Services },
+  { path: "/about-us", name: "about", Component: About }
+];
+
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
 
 function App() {
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+
+  useEffect(() => {
+    // prevents flashing
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+    }, 1000);
+
+    window.addEventListener("resize", debouncedHandleResize);
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header dimensions={dimensions} />
+      <div className="App">
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path}>
+            <Component dimensions={dimensions} />
+          </Route>
+        ))}
+      </div>
+      <Navigation />
+    </>
   );
 }
 
